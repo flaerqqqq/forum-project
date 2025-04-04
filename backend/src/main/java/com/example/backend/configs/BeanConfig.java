8b7e256891fafba05a67250927a33eb96742037d
Vitaliy Verzun <vitaliyverzyn25@gmail.com>
2025-04-04 17:50:58 +0300

    configured mail sender

--- backend/src/main/java/com/example/backend/configs/BeanConfig.java
+++ backend/src/main/java/com/example/backend/configs/BeanConfig.java
@@ -1,14 +1,43 @@
 package com.example.backend.configs;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
 
 @Configuration
 public class BeanConfig {
 
+    @Value("${gmail.username}")
+    private String gmailUsername;
+
+    @Value("${gmail.password}")
+    private String gmailPassword;
+
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(gmailUsername);
+        mailSender.setPassword(gmailPassword);
+
+        Properties props = new Properties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls", "true");
+        props.put("mail.smtp.debug", "true");
+        mailSender.setJavaMailProperties(props);
+
+        return mailSender;
     }
 }
