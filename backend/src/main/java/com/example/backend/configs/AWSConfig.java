61ca5d9e775d01bc51b78414d15d911ce8e1facc
Vitaliy Verzun <vitaliyverzyn25@gmail.com>
2025-04-12 13:24:00 +0300

    content moderation for avatars

--- backend/src/main/java/com/example/backend/configs/AWSConfig.java
+++ backend/src/main/java/com/example/backend/configs/AWSConfig.java
@@ -3,6 +3,8 @@ package com.example.backend.configs;
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.AmazonRekognitionClientBuilder;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,7 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class S3Config {
+public class AWSConfig {
 
     @Value("${aws.secret-key}")
     private String secretKey;
@@ -26,6 +28,16 @@ public class S3Config {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
         AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
         return AmazonS3ClientBuilder.standard()
+                .withCredentials(credentialsProvider)
+                .withRegion(region)
+                .build();
+    }
+
+    @Bean
+    public AmazonRekognition amazonRekognition() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(awsCredentials);
+        return AmazonRekognitionClientBuilder.standard()
                 .withCredentials(credentialsProvider)
                 .withRegion(region)
                 .build();
--- backend/src/main/java/com/example/backend/utils/ImageValidator.java
+++ backend/src/main/java/com/example/backend/utils/ImageValidator.java
@@ -1,16 +1,27 @@
 package com.example.backend.utils;
 
+import com.amazonaws.services.rekognition.AmazonRekognition;
+import com.amazonaws.services.rekognition.model.DetectModerationLabelsRequest;
+import com.amazonaws.services.rekognition.model.DetectModerationLabelsResult;
+import com.amazonaws.services.rekognition.model.Image;
+import com.amazonaws.services.rekognition.model.ModerationLabel;
 import com.example.backend.exceptions.ImageValidationException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
+@RequiredArgsConstructor
 public class ImageValidator {
 
+    private static final float MIN_CONFIDENCE = 75F;
     private static final int MIN_HEIGHT = 250;
     private static final int MAX_HEIGHT = 2000;
     private static final int MIN_WIDTH = 250;
@@ -18,20 +29,22 @@ public class ImageValidator {
     private static final double MIN_ASPECT_RATIO = 0.5;
     private static final double MAX_ASPECT_RATIO = 2;
 
+    private final AmazonRekognition rekognitionClient;
+
     public void validateAvatar(MultipartFile file) {
         try {
             validateFileType(file);
             validateImageDimensions(file);
+            validateImageContentModeration(file);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
     private void validateFileType(MultipartFile file) {
-        if (!file.getContentType().startsWith("image")) {
-            throw new ImageValidationException("The file should be of image type");
+        String contentType = file.getContentType();
+        if (contentType.equals("image/jpg") || contentType.equals("image/png")) {
+            throw new ImageValidationException("The file must be either in a .jpg or a .png format");
         }
     }
 
@@ -50,5 +63,24 @@ public class ImageValidator {
         } else if (aspectRatio > MAX_ASPECT_RATIO) {
             throw new ImageValidationException("Maximum aspect ratio is %s, but given is %s".formatted(MAX_ASPECT_RATIO, aspectRatio));
         }
+    }
+
+    private void validateImageContentModeration(MultipartFile file) throws IOException {
+        Image image = new Image().withBytes(ByteBuffer.wrap(file.getBytes()));
+
+        DetectModerationLabelsRequest request = new DetectModerationLabelsRequest()
+                .withImage(image)
+                .withMinConfidence(MIN_CONFIDENCE);
+
+        DetectModerationLabelsResult result = rekognitionClient.detectModerationLabels(request);
+        List<ModerationLabel> moderationLabels = result.getModerationLabels();
+
+        if (!moderationLabels.isEmpty()) {
+            String labelsString = moderationLabels.stream()
+                    .map(ModerationLabel::getName)
+                    .collect(Collectors.joining(", "));
+            throw new ImageValidationException("The given picture has an inappropriate content: %s".formatted(labelsString));
+        }
+
     }
 }
