ec5d341d545d7421f2a875b885a54c557e169f2c
Vitaliy Verzun <vitaliyverzyn25@gmail.com>
2025-04-14 20:08:46 +0300

    add logout button for header when user is authenticated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,16 @@
-import { Link } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 import { isAuthenticated } from '../utils/auth';
+import Cookies from 'js-cookie';
 
 const Header = () => {
     const auth = isAuthenticated();
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        Cookies.remove('token');
+
+        navigate('/login')
+    }
 
     return (
         <header className="header">
@@ -11,7 +19,10 @@ const Header = () => {
                 {!auth ? (
                     <Link to="/login">Login</Link>
                 ) : (
+                    <>
                     <Link to="/users/me">Profile</Link> // Or the logged-in user's ID
+                    <button onClick={handleLogout()}>Logout</button>
+                    </>
                 )}
             </nav>
         </header>
