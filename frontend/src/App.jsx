911b9c8ecd66a54d809b30ab6e9b3c8bea5d4129
Vitaliy Verzun <vitaliyverzyn25@gmail.com>
2025-04-14 19:36:37 +0300

    layout for auth pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,25 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import EmailVerificationNotice from './pages/EmailVerificationNotice'
 import GuestOnlyRoute from "./routes/GuestOnlyRoute";
+import AuthLayout from "./layouts/AuthLayout";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/register" element={
-                    <GuestOnlyRoute>
-                        <Register />
-                    </GuestOnlyRoute>
-                } />
-                <Route path="/login" element={
-                    <GuestOnlyRoute>
-                        <Login />
-                    </GuestOnlyRoute>
-                } />
-                <Route path="/email-verify-notice" element={<EmailVerificationNotice />} />
+                <Route element={<AuthLayout />}>
+                    <Route path="/register" element={
+                        <GuestOnlyRoute>
+                            <Register />
+                        </GuestOnlyRoute>
+                    } />
+                    <Route path="/login" element={
+                        <GuestOnlyRoute>
+                            <Login />
+                        </GuestOnlyRoute>
+                    } />
+                    <Route path="/email-verify-notice" element={<EmailVerificationNotice />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
