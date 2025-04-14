d89279357120c1ed14d9f5ed35706905cd18b8be
Vitaliy Verzun <vitaliyverzyn25@gmail.com>
2025-04-14 18:33:56 +0300

    route authenticated users to home page if tries to reach /login, /register endpoints

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import EmailVerificationNotice from './pages/EmailVerificationNotice'
+import GuestOnlyRoute from "./routes/GuestOnlyRoute.js";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={
+                    <GuestOnlyRoute>
+                        <Register />
+                    </GuestOnlyRoute>
+                } />
+                <Route path="/login" element={
+                    <GuestOnlyRoute>
+                        <Login />
+                    </GuestOnlyRoute>
+                } />
                 <Route path="/email-verify-notice" element={<EmailVerificationNotice />} />
             </Routes>
         </BrowserRouter>
